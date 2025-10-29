60fe9c2cd3f122d49025b29530886d0a44bf0ae7
Maha6468 <motherma7633@gmail.com>
2025-10-29 23:19:51 +0600

    first commit

---
+++ lib/coffeeHomePage/all_screen/details.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class Details extends StatefulWidget {
+  const Details({super.key});
+
+  @override
+  State<Details> createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> {
+  String selectedSize = "Venti";
+
+  final List<Map<String, String>> sizes = [
+    {"label": "Tall", "volume": "12 Fl Oz"},
+    {"label": "Grande", "volume": "16 Fl Oz"},
+    {"label": "Venti", "volume": "24 Fl Oz"},
+    {"label": "Custom", "volume": "-- Fl Oz"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Center(
+                child: Text(
+                  "Details",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              trailing: Icon(Icons.shopping_bag_outlined, color: Colors.amber),
+            ),
+            SizedBox(height: 60),
+
+            Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Positioned(
+                  height: 320,
+                  bottom: -28,
+                  child: Image.asset(
+                    "assets/images/img.png",
+                    height: 300,
+                    //fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Caramel \nFrappuccino",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "\$30.00",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                      Text("Best Sales",style: TextStyle(fontSize: 11,color: Colors.grey),)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("Size Options",style: TextStyle(color: Colors.grey,fontSize: 18),)),
+            ),
+
+
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: sizes.map((size) {
+                final isSelected = selectedSize == size["label"];
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedSize = size["label"]!;
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color:
+                          isSelected ? Colors.green : Colors.grey.shade200,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            if (isSelected)
+                              BoxShadow(
+                                color: Colors.green.withOpacity(0.3),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                              ),
+                          ],
+                        ),
+                        child: Icon(
+                          Icons.local_cafe,
+                          size: 28,
+                          color: isSelected ? Colors.white : Colors.green,
+                        ),
+                      ),
+
+                      const SizedBox(height: 8),
+
+                      // Text outside the box 👇
+                      Text(
+                        size["label"]!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: isSelected ? Colors.green : Colors.black87,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        size["volume"]!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+
+
+
+            SizedBox(height: 30,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 40,width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle
+                      ),
+                      child: IconButton(
+                          color: Colors.white,
+                          onPressed: (){},
+                          icon: Icon(Icons.remove)),
+                    ),
+                    SizedBox(width: 5,),
+
+                    Text("1",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                    SizedBox(width: 5,),
+                    Container(
+                      height: 40,width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle
+                      ),
+                      child: IconButton(
+                          color: Colors.white,
+                          onPressed: (){},
+                          icon: Icon(Icons.add)),
+                    ),
+
+
+                  ],
+                ),
+
+                SizedBox(
+                  height: 55,width: 180,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green
+                      ),
+                      onPressed: (){},
+                      child:Text("Add to Order",style: TextStyle(color: Colors.white),)),
+                )
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/coffeeHomePage/all_screen/home.dart
@@ -0,0 +1,166 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'details.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ListTile(
+              leading: Icon(Icons.baby_changing_station),
+              title: Text("Qahwa",style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle:Text("Space"),
+              // trailing: Icon(Icons.shopping_bag_outlined),
+              trailing: IconButton(onPressed: (){
+                Navigator.push(context,MaterialPageRoute(builder: (context)=>Details()));
+              }, icon: Icon(Icons.shopping_bag_outlined,color: Colors.amber,size: 35,)),
+            ),
+
+
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("Smooth Out \nYour Everyday",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: size.width * 0.07),),
+              ),
+            ),
+
+
+            Flexible(
+              child: ClipPath(
+                clipper: TopCurveClipper(),
+                child: Container(
+                  width: double.infinity,
+                  color: const Color(0xFF1E4D2B),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Transform.translate(
+                              offset: const Offset(-15, 45),
+                              //offset: Offset(-size.width * 0.04, size.height * 0.05),
+                              child: _buildCategoryItem("☕", "HOT COFFEE",size),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(-4, 12),
+                              // offset: Offset(-size.width * 0.04, size.height * 0.05),
+                              child: _buildCategoryItem("🥤", "DRINKS",size),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(4, 12),
+                              // offset: Offset(-size.width * 0.04, size.height * 0.05),
+                              child: _buildCategoryItem("🍵", "HOT TEAS",size),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(15, 45),
+                              //  offset: Offset(-size.width * 0.04, size.height * 0.05),
+                              child: _buildCategoryItem("🧁", "BAKERY",size),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      Image.asset(
+                        "assets/images/img.png",
+                        // height: 200,
+                        height: size.height * 0.20,
+                        fit: BoxFit.contain,
+                      ),
+                      const Text(
+                        "Caramel \nFrappuccino",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        "\$30.00",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white70,
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class TopCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, 90);
+    path.quadraticBezierTo(
+      size.width / 2,
+      0,
+      size.width,
+      90,
+    );
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
+
+Widget _buildCategoryItem(String icon, String title,Size size) {
+  return Column(
+    children: [
+      CircleAvatar(
+        // radius: 30,
+        radius: size.width * 0.07,
+        backgroundColor: Colors.white,
+        child: Text(icon, style:  TextStyle(
+          // fontSize: 30,
+            fontSize: size.width * 0.07
+
+        )),
+      ),
+      const SizedBox(height: 5),
+      Text(
+        title,
+        style: const TextStyle(
+            fontSize: 9, fontWeight: FontWeight.w600, color: Colors.grey
+        ),
+      ),
+    ],
+  );
+}
